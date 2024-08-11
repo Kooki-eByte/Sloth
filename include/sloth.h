9505359006edd4e29aea77174a0c01b6f1468549
Cristian Hornedo <cristianhornedo@gmail.com>
2024-08-11 01:48:04 -0400

    Updated vec to be capital V and updated methods to be pass by address for memory management

--- include/sloth.h
+++ include/sloth.h
@@ -2,24 +2,24 @@
 #ifndef SLOTH
 #define SLOTH
 
-typedef struct {
+typedef struct Vec2 {
   f32 x;
   f32 y;
-} vec2;
+} Vec2;
 
-typedef struct {
+typedef struct Vec3 {
   f32 x;
   f32 y;
   f32 z;
-} vec3;
+} Vec3;
 
-typedef struct {
+typedef struct Vec4 {
   f32 x;
   f32 y;
   f32 z;
   f32 w;
-} vec4;
+} Vec4;
 
-f32 vec2_dot_product(vec2, vec2);
-f32 vec2_cross_product(vec2, vec2);
+f32 vec2_dot_product(Vec2 *v1, Vec2 *v2);
+f32 vec2_cross_product(Vec2 *v1, Vec2 *v2);
 #endif
--- src/s_math.c
+++ src/s_math.c
@@ -1,5 +1,7 @@
 #include "../include/sloth.h"
 
-f32 vec2_dot_product(vec2 a, vec2 b) { return (a.x * b.x) + (a.y * b.y); }
+f32 vec2_dot_product(Vec2 *a, Vec2 *b) { return (a->x * b->x) + (a->y * b->y); }
 
-f32 vec2_cross_product(vec2 a, vec2 b) { return (a.x * b.y) - (a.y * b.x); }
+f32 vec2_cross_product(Vec2 *a, Vec2 *b) {
+  return (a->x * b->y) - (a->y * b->x);
+}
